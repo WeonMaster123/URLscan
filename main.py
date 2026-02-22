ab831e88a7649b4f330592c2fd6e94a5d37625ba
WeonMaster3000 <maximomdalidet@gmail.com>
2026-02-21 21:05:44 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from analyzer import analyzer
 def main():
     parser = argparse.ArgumentParser(description="Tool for analizer url suspicious")
     parser.add_argument("url",help="Your url suspicious")
+    parser.add_argument("-H",help="look HTML of the page",action="store_true")
 
     args = parser.parse_args()
 
-    analyzer(args.url)
+    analyzer(args.url,args.H)
 
 main()
