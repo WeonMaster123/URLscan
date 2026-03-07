a43970ad6f42c4a352022c0d03a5d1277e0ac341
CiberRino <maximomdalidet@gmail.com>
2026-03-07 16:57:36 -0300

    Add files via upload

--- analyzer.py
+++ analyzer.py
@@ -8,6 +8,8 @@ def analyzer(URL,H,ss):
 
     sign = 0
     total_score = 0
+    suspicious_words = ["secure","verify","account","bank","login","update"]
+    found = []
 
     try:
 
@@ -19,12 +21,19 @@ def analyzer(URL,H,ss):
         pattern = f"https?://({domain})/[a-zA-Z0-9]+"
         patternIp4 = r'\b(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\b'
 
+        for word in suspicious_words:
+            if word in URL:
+                found.append(word)
+                total_score += 5
+                sign += 1
+        print(f"[*] suspicous words detected: {found}")
+
         if ext:
             lista_subdominios = ext.split('.')
             count_subdomain = len(lista_subdominios)
 
             if count_subdomain >= 4:
-                total_score += 5
+                total_score += 5 
                 sign += 1 
                 print(f"[!] too much subdomains: {count_subdomain}")
 
--- scoring.py
+++ scoring.py
@@ -1,9 +1,10 @@
 def score(signs,score):
 
-    if score <= 15:
+    if score <= 20:
         print(f"[*] possible low risk {signs} possible signs of phising. Total score {score}")
-    elif score <= 25:
-        print(f"[:] possible mid risk {signs} possible signs of phising. Total score {score}")
     elif score <= 45:
+        print(f"[:] possible mid risk {signs} possible signs of phising. Total score {score}")
+    elif score <= 70:
         print(f"[!] possible hight risk {signs} possible signs of phising. Total score {score}")
+    
     
